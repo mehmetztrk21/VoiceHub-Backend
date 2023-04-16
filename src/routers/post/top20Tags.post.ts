14d6fbbb04c1eb7ab45b66e87b2a39d7de6dd691
mehmetztrk21 <mehmet.ozturk@dincerlojistik.com>
2023-04-16 14:54:58 +0300

    bug-fix

--- src/routers/post/top20Tags.post.ts
+++ src/routers/post/top20Tags.post.ts
@@ -10,10 +10,10 @@ export default async function ({ body, voiceHubDb, req, session }: AppContext<Re
     if (!resolved) return response.setError("Unauthorized");
     const user = await mongoDb.collection("users").findOne({ _id: new ObjectId(resolved["_id"]) });
     if (!user) return response.setError("Unauthorized");
-    const pastWeek = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000); // Calculate past week date
+   // const pastWeek = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000); // Calculate past week date
     const topCategories = await mongoDb.collection("posts")
         .aggregate([
-            { $match: { createdAt: { $gte: pastWeek } } }, // Filter posts created in past week
+          //  { $match: { createdAt: { $gte: pastWeek } } }, // Filter posts created in past week
             { $unwind: "$categories" }, // Split categories array into multiple documents
             { $group: { _id: "$categories", count: { $sum: 1 } } }, // Group by category and count the occurrences
             { $sort: { count: -1 } }, // Sort by count in descending order
