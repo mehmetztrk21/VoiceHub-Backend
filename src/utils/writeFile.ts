1f745987a6879d78ed625d3e3c3bced9a10d3cbd
mehmetztrk21 <mehmet.ozturk@dincerlojistik.com>
2023-04-25 16:09:40 +0300

    remove profile photo or description voice service

--- src/utils/writeFile.ts
+++ src/utils/writeFile.ts
@@ -4,3 +4,4 @@ import util from 'util';
 
 export const writeFile = util.promisify(fs.writeFile);
 export const readFile = util.promisify(fs.readFile);
+export const removeFile = util.promisify(fs.unlink);
