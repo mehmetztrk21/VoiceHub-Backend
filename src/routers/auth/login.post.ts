71b6c1d3c90ff1e6dddfbea470d67ae9a73efac7
mehmetztrk21 <mehmet.ozturk@dincerlojistik.com>
2023-05-03 10:47:07 +0300

    updated

--- src/routers/auth/login.post.ts
+++ src/routers/auth/login.post.ts
@@ -8,16 +8,21 @@ interface Request {
     password: string;
 }
 export default async function ({ body, session, jwt, voiceHubDb, req }: AppContext<Request>) {
-    const mongoDb = await voiceHubDb.db("voiceHub");
-    const user = await mongoDb.collection("users").findOne({ $and: [{ username: body.username }, { password: md5(body.password) }] }, { projection: { password: 0, descriptionVoiceInfo: 0, profilePhotoInfo: 0 } });
+    const mongoDb = voiceHubDb.db("voiceHub");
+    const user = await mongoDb.collection("users").findOne({
+        $and: [{ username: body.username }, {
+            password: md5(body.password)
+        }]
+    },
+        { projection: { password: 0, descriptionVoiceInfo: 0, profilePhotoInfo: 0 } });
     if (user) {
-        const generatedToken = await jsonwebtoken.sign({ _id: user._id }, jwt.secret, { expiresIn: "100y" });
+        const generatedToken = jsonwebtoken.sign({ _id: user._id }, jwt.secret, { expiresIn: "100y" });
         session.granted = true;
         session.token = generatedToken;
         session.user = user;
         return new ApiResponse().setSuccess({
             accessToken: generatedToken,
-            user: {...user}
+            user: { ...user }
         });
     }
     else {
