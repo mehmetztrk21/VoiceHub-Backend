5716c5d349e86024c415cc686800c5dc859637bf
mehmetztrk21 <mehmet.ozturk@dincerlojistik.com>
2023-05-04 16:35:49 +0300

    explore filter by passive user

--- src/routers/post/explorePosts.post.ts
+++ src/routers/post/explorePosts.post.ts
@@ -40,7 +40,8 @@ export default async function ({ body, voiceHubDb, req, session }: AppContext<Re
                                 $expr: {
                                     $and: [
                                         { $eq: ["$postId", "$$postId"] },
-                                        { $eq: ["$isDeleted", false] }
+                                        { $eq: ["$isDeleted", false] },
+                                        { "$not": { "$in": ["$createdBy", user.blockedUsers] } }
                                     ]
                                 }
                             }
@@ -50,7 +51,18 @@ export default async function ({ body, voiceHubDb, req, session }: AppContext<Re
                                 from: "users",
                                 localField: "createdBy",
                                 foreignField: "_id",
-                                as: "createdBy"
+                                as: "createdBy",
+                                pipeline: [
+                                    {
+                                        $match: {
+                                            $expr: {
+                                                $and: [
+                                                    { $eq: ["$status", "active"] }
+                                                ]
+                                            }
+                                        }
+                                    }
+                                ]
                             }
                         },
                         { $unwind: "$createdBy" },
@@ -78,8 +90,20 @@ export default async function ({ body, voiceHubDb, req, session }: AppContext<Re
                     from: "users",
                     localField: "createdBy",
                     foreignField: "_id",
-                    as: "createdBy"
+                    as: "createdBy",
+                    pipeline: [
+                        {
+                            $match: {
+                                $expr: {
+                                    $and: [
+                                        { $eq: ["$status", "active"] }
+                                    ]
+                                }
+                            }
+                        }
+                    ]
                 }
+
             },
             { $unwind: "$createdBy" },
             {
