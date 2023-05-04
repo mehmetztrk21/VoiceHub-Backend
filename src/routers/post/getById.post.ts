14ab6c9ba6630471cca4546000c92d14c57010fe
mehmetztrk21 <mehmet.ozturk@dincerlojistik.com>
2023-05-04 14:49:49 +0300

    post getById filter block user

--- src/routers/post/getById.post.ts
+++ src/routers/post/getById.post.ts
@@ -2,10 +2,14 @@ import { ApiResponse } from "fastapi-next";
 import { ObjectId } from "mongodb";
 import { AppContext } from "../../AppContext";
 import { resolveToken } from "../../utils/resolveToken";
-
+import * as yup from "yup";
 interface Request {
     postId: string;
 }
+
+export const validate = yup.object().shape({
+    postId: yup.string().required()
+});
 
 export default async function ({ body, voiceHubDb, req, session }: AppContext<Request>) {
     var response = new ApiResponse();
@@ -14,7 +18,7 @@ export default async function ({ body, voiceHubDb, req, session }: AppContext<Re
     if (!resolved) return response.setError("Unauthorized");
     const user = await mongoDb.collection("users").findOne({ _id: new ObjectId(resolved["_id"]) });
     if (!user) return response.setError("Unauthorized");
-    const post = await mongoDb.collection("posts").findOne({ _id: new ObjectId(body.postId) });
+    const post = await mongoDb.collection("posts").findOne({ _id: new ObjectId(body.postId), createdBy: { $nin: user.blockedUsers } });
     if (!post) return response.setError("Post not found");
     return response.setSuccess(post);
 }
